cd255e003e2e8716f6a603cc288bfdcc6f71534a
Floopsee <TheMattkeith@yahoo.com>
2023-07-03 09:25:45 -0400

    fruit search first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const input = document.querySelector('#fruit');
+const suggestions = document.querySelector('.suggestions ul');
+const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+
+document.addEventListener("keyup", search = (e) => {
+	if (e.key === "Enter") {
+		input.value = ''
+		while (suggestions.firstChild) suggestions.removeChild(suggestions.firstChild);
+	}
+})
+
+input.addEventListener("keyup", search = () => {
+	while (suggestions.firstChild) suggestions.removeChild(suggestions.firstChild);
+	let inputVal = input.value.toLowerCase()
+	let results = [];
+	for (let i of fruit) {
+		if (i.toLowerCase().includes(inputVal.toLowerCase()) && input.value !== '') {
+			let suggestion = document.createElement("li")
+			suggestion.setAttribute("id", "options")
+
+			suggestion.innerHTML = i.toLowerCase().replace(inputVal, "<b>" + inputVal + "</b>")
+
+			suggestions.appendChild(suggestion)
+			suggestion.addEventListener("click", function () {
+				input.value = suggestion.innerText
+				while (suggestions.firstChild) suggestions.removeChild(suggestions.firstChild);
+			})
+		}
+	}
+	return results;
+})
